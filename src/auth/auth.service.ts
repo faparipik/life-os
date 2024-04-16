928e670676fd99e15b9e0bd914e1a7663ea079e5
Faruk Bruncevic <bruncevicfaruk@gmail.com>
2024-04-16 14:51:41 +0200

    sign in user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@nestjs/common';
-
-@Injectable({})
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2';
+import { AuthDto } from './dto';
+@Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   signin() {
     return 'signin';
   }
 
-  signup() {
-    return 'signup';
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+
+    delete user.hash;
+
+    return user;
   }
 }
