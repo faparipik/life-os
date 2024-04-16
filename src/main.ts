3e2ac06776b05fc1491886a44a25c36646a34c08
Faruk Bruncevic <bruncevicfaruk@gmail.com>
2024-04-16 17:25:59 +0200

    added comments

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // we want to validate every request
+  // whitelist is to remove data from the request if data is not specify in DTO
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
