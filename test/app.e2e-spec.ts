6f09d6a6408421dea222b65fb9478ac4da89dd81
Faruk Bruncevic <bruncevicfaruk@gmail.com>
2024-04-17 19:49:36 +0200

    e2e Tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,64 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test } from '@nestjs/testing';
+import { AppModule } from '../src/app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
-describe('AppController (e2e)', () => {
+import * as pactum from 'pactum';
+import { AuthDto } from 'src/auth/dto';
+
+describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
+
     await app.init();
+
+    await app.listen('3333');
+
+    prisma = app.get(PrismaService);
+
+    await prisma.cleanDb();
+    pactum.request.setBaseUrl('http://localhost:3333');
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('Auth', () => {
+    const dto: AuthDto = {
+      email: 'neki@email.com',
+      password: 'nekipassword',
+    };
+    describe('SignUP', () => {
+      it('should signed up', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+    describe('SignIn', () => {
+      it('should signin', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+  });
+
+  afterAll(() => {
+    app.close();
   });
 });
